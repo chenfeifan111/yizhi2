6fdcae73f145f76b50af046cdc2f04afef11c9f8
shisijin <784372659@qq.com>
2025-03-21 14:07:04 +0800

    1

--- src/api/shareMgr.js
+++ src/api/shareMgr.js
@@ -1,8 +1,10 @@
 import {request} from '@/utils/http'
 
 const API = {
-    checkCustomerLink: '/shareMgr/checkCustomerLink',
-    addPlayerClickCnt: '/shareMgr/recordClickCnt'
+    // checkCustomerLink: '/shareMgr/checkCustomerLink',
+    // addPlayerClickCnt: '/shareMgr/recordClickCnt'
+    checkCustomerLink: '/customer',
+    addPlayerClickCnt: '/addPlayer'
 }
 // * 验证客服分享链接是否有效
 export const checkCustomerLinkApi = (data) =>
