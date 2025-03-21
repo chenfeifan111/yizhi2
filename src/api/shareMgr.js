b5c75937a26b90c02d66140d4776a612bfa6633d
shisijin <784372659@qq.com>
2025-03-21 14:45:02 +0800

    1

--- src/api/shareMgr.js
+++ src/api/shareMgr.js
@@ -9,7 +9,7 @@ const API = {
 // * 验证客服分享链接是否有效
 export const checkCustomerLinkApi = (data) =>
     // request.post(API.checkCustomerLink, data)
-    fetch('https://yizhi2.pages.dev/'+API.checkCustomerLink, {
+    fetch('https://yizhi2.pages.dev'+API.checkCustomerLink, {
         method: 'POST', // HTTP 方法
         headers: {
             'Content-Type': 'application/json', // 设置请求体格式为 JSON
@@ -27,7 +27,7 @@ export const checkCustomerLinkApi = (data) =>
 // * 点解facebook导量界面，增加玩家点击次数
 export const addPlayerClickCntApi = (data) =>
 // request.post(API.addPlayerClickCnt, data)
-    fetch('https://yizhi2.pages.dev/'+API.addPlayerClickCnt, {
+    fetch('https://yizhi2.pages.dev'+API.addPlayerClickCnt, {
         method: 'POST', // HTTP 方法
         headers: {
             'Content-Type': 'application/json', // 设置请求体格式为 JSON
