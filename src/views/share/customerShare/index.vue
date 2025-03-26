282b651c82d3659e692411f92b97c4e27c9903b2
shisijin <784372659@qq.com>
2025-03-26 17:15:28 +0800

    2

--- src/views/share/customerShare/index.vue
+++ src/views/share/customerShare/index.vue
@@ -61,6 +61,7 @@ onMounted(() => {
 const onOpenThisPage = () => {
   sCustomerId.value = route.query?.customerId ? route.query.customerId : "";
   sCustomerName.value = route.query?.customer ? route.query.customer : "";
+  console.log(sCustomerId.value,sCustomerName.value)
   onCheckCustomerLink();
 };
 
