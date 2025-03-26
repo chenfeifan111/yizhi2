efad1f0c0bca297a628d96e4744098d32f4cb545
shisijin <784372659@qq.com>
2025-03-26 15:00:30 +0800

    2

--- src/utils/PostApi.js
+++ src/utils/PostApi.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 export const postApi = (path, data) => {
     console.log(import.meta.env.VITE_ENV)
-    return axios.post('/public', {
-        path: "/web_client" + path,
-        env: import.meta.env.VITE_ENV,
-        data: data
-    })
+    if (import.meta.env.VITE_ENV==="prod"){
+        return axios.post('/public', {
+            path: "/web_client" + path,
+            env: import.meta.env.VITE_ENV,
+            data: data
+        })
+    }else if (import.meta.env.VITE_ENV==="dev"){
+        return axios.post('http://localhost:8787', {
+            path: "/web_client" + path,
+            env: import.meta.env.VITE_ENV,
+            data: data
+        })
+    }
 };
